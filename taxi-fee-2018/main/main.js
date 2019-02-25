988d84233fb86a68470462836a314caffd7f36d5
Ruifeng-Wu <296078293@qq.com>
2019-02-25 19:24:12 +0800

    implement test2

--- taxi-fee-2018/main/main.js
+++ taxi-fee-2018/main/main.js
@@ -4,6 +4,9 @@ module.exports = function main(mileage, time) {
     if (mileage <= 2) {
         fee = 6;
     }
+    if (mileage > 2 && mileage <= 8) {
+        fee = 6 + Math.ceil(mileage - 2) * 0.8;
+    }
     fee = Math.round(time * 0.25 + fee);
     return fee;
 };
