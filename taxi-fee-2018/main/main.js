608754e015c65c90d980939cab47d28ea29d9349
Ruifeng-Wu <296078293@qq.com>
2019-02-25 19:41:56 +0800

    refactoring
    remove duplicated code

--- taxi-fee-2018/main/main.js
+++ taxi-fee-2018/main/main.js
@@ -1,14 +1,11 @@
 module.exports = function main(mileage, time) {
     // console.log("Debug Info");
-    let fee = 0;
-    if (mileage <= 2) {
-        fee = 6;
-    }
-    if (mileage > 2 && mileage <= 8) {
-        fee = 6 + Math.ceil(mileage - 2) * 0.8;
+    let fee = 6;
+    if (mileage > 2) {
+        fee += Math.ceil(mileage - 2) * 0.8;
     }
     if (mileage > 8) {
-        fee = 6 + 6 * 0.8 + Math.ceil(mileage - 8) * 0.8 * 1.5;
+        fee += Math.ceil(mileage - 8) * 0.5;
     }
     fee = Math.round(time * 0.25 + fee);
     return fee;
